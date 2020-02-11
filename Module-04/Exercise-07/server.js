66f0414523390eebbca890fb5450d9763184735b
Victor <thadeucity@gmail.com>
2020-02-10 21:56:44 -0300

    Module 04 exercise 7

---
+++ Module-04/Exercise-07/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const methodOverride = require('method-override');
+
+const routes = require('./routes');
+
+const server = express();
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static('public'));
+server.use(methodOverride('_method'));
+server.use(routes);
+
+server.set('view engine', 'njk');
+
+nunjucks.configure('./views', {
+  express:server,
+  autoescape: false,
+  noCache: true
+});
+
+server.listen(5000, function() {
+  console.log('server is up and running')
+});
+
+
+//npm init -y
+//npm install express
+//npm install -D nodemon. 
+//npm install nunjucks
+//npm install browser-sync npm-run-all -D
+//npm install method-override
---
+++ Module-04/Exercise-07/utils.js
@@ -0,0 +1,38 @@
+const Intl = require ('intl');
+
+module.exports = {
+  age: function (timestamp) {
+    const today = new Date();
+    const birth = new Date(timestamp);
+
+    let age = today.getUTCFullYear() - birth.getUTCFullYear();
+    
+    if (
+      today.getUTCMonth() < birth.getUTCMonth() ||
+        today.getUTCMonth() == birth.getUTCMonth() &&
+        today.getUTCDate() < birth.getUTCDate()
+      ){
+      age--;
+    }
+
+    return age;
+  },
+  convertDate: function (timestamp) {
+    return (new Intl.DateTimeFormat('pt-BR').format(timestamp));
+  },
+  date: function (timestamp){
+    const birth = new Date(timestamp);
+
+    const year = birth.getUTCFullYear();
+    const month = `0${birth.getUTCMonth()}`.slice(-2);
+    const day = `0${birth.getUTCDate()}`.slice(-2);
+
+    return {
+      iso: `${year}-${month}-${day}`,
+      birthDay: `${day}/${month}`,
+      day,
+      month,
+      year
+    }
+  }
+}
