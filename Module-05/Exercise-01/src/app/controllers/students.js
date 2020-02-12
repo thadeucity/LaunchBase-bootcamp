4b99c4ab852c7e8ae3aa7a8996b9448ebb9c322d
Victor <thadeucity@gmail.com>
2020-02-12 09:11:45 -0300

    Module 05 - Ex01

---
+++ Module-05/Exercise-01/src/app/controllers/students.js
@@ -0,0 +1,166 @@
+const fs = require('fs');
+const data = require('./data.json');
+const { age, convertDate, date } = require('../../lib/utils');
+
+const studentDegrees = ['Kindergarten', '1st Grade', '2nd Grade', '3rd Grade',
+  '4th Grade', '5th Grade', '6th Grade', '7th Grade', '8th Grade', 'Freshman',
+  'Sophomore', 'Junior', 'Senior'];
+
+
+module.exports = {
+  index(req, res){
+    let students = [];
+    let student = {};
+  
+    for (pupil of data.pupils){
+      student = {
+        ...pupil
+      }
+      students.push(student);
+    }
+  
+    return res.render('students/index', {students});
+  },
+
+  show(req, res){
+    const{ id } = req.params;
+  
+    const foundStudent = data.pupils.find(function(student){
+      return student.id == id;
+    });
+  
+    if (!foundStudent) return res.send("This pupil was not found");
+  
+    let teachPlace = [];
+  
+    if (foundStudent.place == 'both'){
+      teachPlace = ['Online', 'In Person'];
+    } else {
+      teachPlace = [foundStudent.place];
+    }
+  
+    const student = {
+      ...foundStudent,
+      birthDay: date(foundStudent.birth).birthDay,
+      degree: foundStudent.degree_lvl,
+      place: teachPlace,
+      created_at: convertDate(foundStudent.created_at)
+    }
+  
+    return res.render('students/show', { student }); 
+  },
+
+  create(req, res) {
+    return res.render('students/create', {studentDegrees});
+  },
+
+  post(req, res){
+
+    const keys = Object.keys(req.body);
+  
+    
+    for (key of keys){
+      if(req.body[key] == ""){
+        return res.send('Please, fill all fields');
+      }
+    }
+  
+    let {
+      avatar_url,
+      name,
+      email,
+      birth,
+      degree_lvl,
+      place,
+      load
+    } = req.body;
+  
+    birth = Date.parse(birth);
+  
+    let id = 1;
+    const lastStudent = data.pupils[data.pupils.length - 1];
+  
+    if (lastStudent){
+      id = lastStudent.id + 1;
+    }
+  
+    data.pupils.push({
+      id,
+      avatar_url,
+      name,
+      email,
+      birth,
+      degree_lvl,
+      place,
+      load
+    });
+  
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+      if (err) return res.send('Write file error!');
+  
+      return res.redirect('/students');
+    });
+  
+  },
+
+  edit(req, res){
+    const{ id } = req.params;
+  
+    const foundStudent = data.pupils.find(function(student){
+      return student.id == id;
+    });
+  
+    if (!foundStudent) return res.send("This pupil was not found");
+  
+    let student = {
+      ...foundStudent,
+      birth: date(foundStudent.birth).iso
+    }
+  
+    return res.render('students/edit', { student, studentDegrees });
+  },
+
+  put(req, res){
+    const { id } = req.body;
+    let index = 0;
+  
+    const foundStudent = data.pupils.find(function(student, foundIndex){
+      if (id == student.id){
+        index = foundIndex;
+        return true;
+      }
+    });
+  
+    if (!foundStudent) return res.send("This pupil was not found");
+  
+    const student = {
+      ...foundStudent,
+      ...req.body,
+      birth: Date.parse(req.body.birth)
+    }
+  
+    data.pupils[index] = student;
+  
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err) {
+      if(err) return res.send("Write error");
+  
+      return res.redirect(`/students/${id}`)
+    });
+  },
+
+  delete(req, res){
+    const { id } = req.body;
+  
+    const filteredStudents = data.pupils.filter(function(student){
+      return student.id != id;
+    });
+  
+    data.pupils = filteredStudents;
+  
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+      if (err) return res.send("Error writing file");
+  
+      return res.redirect("/students");
+    });
+  }
+}
---
+++ Module-05/Exercise-01/src/lib/utils.js
@@ -0,0 +1,39 @@
+const Intl = require ('intl');
+
+module.exports = {
+  age(timestamp) {
+    const today = new Date();
+    const birth = new Date(timestamp);
+
+    let age = today.getUTCFullYear() - birth.getUTCFullYear();
+    
+    if (
+      today.getUTCMonth() < birth.getUTCMonth() ||
+        today.getUTCMonth() == birth.getUTCMonth() &&
+        today.getUTCDate() < birth.getUTCDate()
+      ){
+      age--;
+    }
+
+    return age;
+  },
+  convertDate(timestamp) {
+    return (new Intl.DateTimeFormat('pt-BR').format(timestamp));
+  },
+  date(timestamp){
+    const birth = new Date(timestamp);
+
+    const year = birth.getUTCFullYear();
+    const month = `0${birth.getUTCMonth()}`.slice(-2);
+    const day = `0${birth.getUTCDate()}`.slice(-2);
+
+    return {
+      iso: `${year}-${month}-${day}`,
+      birthDay: `${day}/${month}`,
+      format: `${day}/${month}/${year}`,
+      day,
+      month,
+      year
+    }
+  }
+}
