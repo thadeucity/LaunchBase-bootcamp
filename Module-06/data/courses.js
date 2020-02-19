361bba974d8daf0da9e3273b1c17cfa7202ccdeb
Victor <thadeucity@gmail.com>
2020-02-19 18:04:43 -0300

    Module 06

---
+++ Module-06/data/courses.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    name: 'starter', 
+    index: 1, 
+    text:"<em>Torne-se um programador desejado</em><br>no mercado com esses cursos gratuitos",
+    modules: 11,
+    price: "Gratuito"
+  },{
+    name: 'launchbase', 
+    index: 2, 
+    text:"<em>Domine programação do zero</em><br>e tenha acesso às melhores oportunidades do mercado",
+    modules: 21,
+    price: "Consultar Valores"
+  },{
+    name: 'gostack', 
+    index: 3, 
+    text:"<em>Treinamento imersivo</em><br>nas tecnologias mais modernas de desenvolvimento web e mobile",
+    modules: 22,
+    price: "Consultar Valores"
+  }
+  
+];
